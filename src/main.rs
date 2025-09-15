394ed0f81b19a4f0ead43f596752d45db5985515
samuel-bazinet <bazinet.samuel1@gmail.com>
2025-09-14 21:54:12 -0400

    Added reviews

--- src/main.rs
+++ src/main.rs
@@ -17,13 +17,18 @@ async fn wild(Path(path): Path<String>) -> Html<String> {
     Html(format!("Coming from {path}"))
 }
 
+async fn reviews(Path(path): Path<String>) -> Html<String> {
+    log::info!("Serving Review {path}");
+    Html(read_to_string(format!("./pages/reviews/{path}.html")).unwrap_or(format!("Review could not be found")))
+}
+
 #[tokio::main]
 async fn main() {
     SimpleLogger::new().init().unwrap();
     log::info!("Starting up webby server");
 
-    let app = Router::new().route("/", get(handler)).route("/{*key}", get(wild));
+    let app = Router::new().route("/", get(handler)).route("/review/{*key}", get(reviews)).route("/{*key}", get(wild));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:80").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
